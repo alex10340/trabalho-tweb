3488cc5e76ead3aee4aa7f044e1428fbe71d18ce
boogie531 <alexandre.pir.silva@gmail.com>
2023-12-24 18:05:41 +0000

    javascript janela 1

--- logica.js
+++ logica.js
@@ -2,6 +2,11 @@ const overlay = document.querySelector('#overlay')
 const donateButton = document.querySelector('#donateButton')
 const paymentDisplay = document.querySelector('#paymentDisplay')
 const paymentDisplayClose = document.querySelector('#paymentDisplayClose')
+const button20 = document.querySelector('#button20')
+const button40 = document.querySelector('#button40')
+const button80 = document.querySelector('#button80')
+const customAmountInput = document.querySelector('#customAmountInput')
+
 
 donateButton.addEventListener('click', function(){
   overlay.style.display='block'
@@ -12,3 +17,55 @@ paymentDisplayClose.addEventListener('click', function(){
   overlay.style.display='none'
   paymentDisplay.style.display='none'
 })
+
+
+button20.addEventListener('click',function(){
+  button20.classList.toggle('buttonSelected')
+
+  if(button40.classList.contains('buttonSelected')){
+    button40.classList.remove('buttonSelected')
+  }
+
+  if(button80.classList.contains('buttonSelected')){
+    button80.classList.remove('buttonSelected')
+  }
+})
+
+button40.addEventListener('click',function(){
+  button40.classList.toggle('buttonSelected')
+
+  if(button20.classList.contains('buttonSelected')){
+    button20.classList.remove('buttonSelected')
+  }
+
+  if(button80.classList.contains('buttonSelected')){
+    button80.classList.remove('buttonSelected')
+  }
+})
+
+button80.addEventListener('click',function(){
+  button80.classList.toggle('buttonSelected')
+
+  if(button20.classList.contains('buttonSelected')){
+    button20.classList.remove('buttonSelected')
+  }
+
+  if(button40.classList.contains('buttonSelected')){
+    button40.classList.remove('buttonSelected')
+  }
+})
+
+customAmountInput.addEventListener('click',function(){
+  if(button20.classList.contains('buttonSelected')){
+    button20.classList.remove('buttonSelected')
+  }
+
+  if(button40.classList.contains('buttonSelected')){
+    button40.classList.remove('buttonSelected')
+  }
+  
+  if(button80.classList.contains('buttonSelected')){
+    button80.classList.remove('buttonSelected')
+  }
+})
+
