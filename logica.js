aa0a61d87206517076fe281108b36082c0726abf
boogie531 <alexandre.pir.silva@gmail.com>
2023-12-24 19:07:16 +0000

    javascript janela 1 (fim)

--- logica.js
+++ logica.js
@@ -1,13 +1,18 @@
 const overlay = document.querySelector('#overlay')
 const donateButton = document.querySelector('#donateButton')
 const paymentDisplay = document.querySelector('#paymentDisplay')
-const paymentDisplay2 = document.querySelector('#paymentDisplay2')
+const paymentDisplayTitle = document.querySelector('#paymentDisplayTitle')
 const paymentDisplayClose = document.querySelector('#paymentDisplayClose')
 const button20 = document.querySelector('#button20')
 const button40 = document.querySelector('#button40')
 const button80 = document.querySelector('#button80')
 const customAmountInput = document.querySelector('#customAmountInput')
 const seguinte = document.querySelector('#seguinte')
+const quantiaInválida = document.querySelector('#quantiaInválida')
+const tipoDoacao = document.querySelector('#tipoDoacao')
+const tipoInvalido = document.querySelector('#tipoInvalido')
+var doacaoUnica = document.querySelector('#doacaoUnica')
+var doacaoMensal = document.querySelector('#doacaoMensal')
 
 
 donateButton.addEventListener('click', function(){
@@ -74,6 +79,21 @@ customAmountInput.addEventListener('click',function(){
   }
 })
 
+var tipoDoacaoSelect
+
+doacaoUnica.addEventListener('change',function(){
+  if(doacaoUnica.checked){
+    doacaoMensal.checked = false
+    tipoDoacaoSelect = 'unica'
+  }
+})
+
+doacaoMensal.addEventListener('change',function(){
+  if(doacaoMensal.checked){
+    doacaoUnica.checked = false
+    tipoDoacaoSelect = 'mensal'
+  }
+})
 
 seguinte.addEventListener('click',function(){
   var quantia
@@ -92,7 +112,28 @@ seguinte.addEventListener('click',function(){
   }
   
   console.log(quantia)
+  
 
-  paymentDisplay.style.display='none'
-  paymentDisplay2.style.display='block'
+
+  if(quantia <= 0 || quantia==''){
+    quantiaInválida.style.display='block'
+  }
+  
+  else if(doacaoUnica.checked == false && doacaoMensal.checked == false){
+    tipoInvalido.style.display='block'
+  }
+
+  else{
+    quantiaInválida.style.display='none'
+    tipoInvalido.style.display='none'
+    tipoDoacao.style.display='none'
+    button20.style.display='none'
+    button40.style.display='none'
+    button80.style.display='none'
+    customAmountInput.style.display='none'
+    paymentDisplayTitle.innerHTML='Informação'
+    
+
+  }
+
 })
